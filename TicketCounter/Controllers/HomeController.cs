0095db8432766be24ae1eeb588c9e86f69edfa92
meringue <sitaulaabhishek7169@gmail.com>
2025-02-11 12:25:17 +0545

    Added drop down menu sorting

--- TicketCounter/Controllers/HomeController.cs
+++ TicketCounter/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ public class HomeController : Controller
         this.Repo = repo;
     }
 
-    public IActionResult Index(string distributor, string searchQuery)
+    public IActionResult Index(string distributor, string searchQuery, string sort)
     {
         var tickets = Repo.Tickets;
 
@@ -17,11 +17,43 @@ public class HomeController : Controller
         {
             tickets = tickets.Where(p => p.Distributor == distributor);
         }
-        
+
         if (!string.IsNullOrEmpty(searchQuery))
         {
             tickets = tickets.Where(p => p.Title.Contains(searchQuery) || p.Distributor.Contains(searchQuery));
         }
+
+        if (!string.IsNullOrEmpty(sort))
+        {
+            string[] condition = sort.Split('-');
+
+            if (condition[0].ToLower() == "price")
+            {
+                if (condition[1].ToLower() == "descending")
+                {
+                    tickets = tickets.OrderByDescending(p => p.Price);
+                }
+
+                else
+                {
+                    tickets = tickets.OrderBy(p => p.Price);
+                }
+            }
+
+            else
+            {
+                if (condition[1].ToLower() == "descending")
+                {
+                    tickets = tickets.OrderByDescending(p => p.Date);
+                }
+
+                else
+                {
+                    tickets = tickets.OrderBy(p => p.Date);
+                }
+            }
+        }
+
         return View(tickets);
     }
 }
--- TicketCounter/obj/Debug/net8.0/TicketCounter.AssemblyInfo.cs
+++ TicketCounter/obj/Debug/net8.0/TicketCounter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketCounter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a04ff940d8a2262e7985be4ff2a66631e5fe8ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4cebc010e5481f9649f3e0c0eadebab39221f5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketCounter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketCounter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
